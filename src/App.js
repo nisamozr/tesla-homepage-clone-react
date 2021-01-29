7d2f50b8215931ea753520f6fd0bd08798e38471
nisamozr <codeexplorkl10@gmail.com>
2021-01-29 10:53:07 +0530

    header +asset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hhdh</h1>
+      {/* header */}
+      <Header/>
+
       
     </div>
   );
